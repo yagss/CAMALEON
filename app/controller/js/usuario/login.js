194831daf0fb3f5ca817f81de6b4b12e190f55c1
Mr. Juan Andres <mr.juan.andres@gmail.com>
2015-08-18 02:24:10 -0500

    Login (antes llamado como "inicio")
    Estos archivos hacen referencia a los anteriores llamados "inicio",
    por la restructuración de directorios en el arbol del proyecto los
    archivos se renombran y actualizan para su uso.

---
+++ app/controller/js/usuario/login.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+
+$(document).ready(function(){
+
+    $(".fieldbox").animateTextbox();
+    
+    $(".icon-enter").click(function(){
+        $("#login").fadeToggle('slow','swing');
+    });
+    
+    // result action
+    var reload = function(result, msg){
+        
+        if(result){
+            
+            //if(msg != ""){alert(msg)}
+            document.location.href='/';
+            
+        }else{
+            
+            alert("error","fa fa-times",msg);
+            
+        }
+        
+    };
+    
+    // form send
+    var beforeSend = function(){
+        
+        $(".cover").addClass("show");
+        $("#login").find(".button").prop("disabled", true);
+        
+    };
+    
+    var complete = function(){
+        
+        $(".cover").removeClass("show");
+        var btn_enable = function(){$("#login").find(".button").prop("disabled", false);};
+        setTimeout(btn_enable, 5000);
+        
+    };
+    
+	$("#login").sendForm({operation:reload, beforeSend: beforeSend, complete: complete});
+
+});

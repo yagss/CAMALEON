ad483cf4543dfeaf05d7e4b3f00573e81f53425f
Mr. Juan Andres <mr.juan.andres@gmail.com>
2015-09-24 10:46:01 -0500

    Corrección Commit
    Se restablecio el archivo index.js.

--- app/controller/js/index.js
+++ app/controller/js/index.js
@@ -1,5 +1,3 @@
-// JavaScript Document
-
 $(document).ready(function(){
     
     $(".notifications").cNotification();
@@ -21,13 +19,14 @@ $(document).ready(function(){
     $("#session").session({container: $("#contenedor"), login: '/app/view/html/usuario/login.html'});
 
     var action = function(){
-        $("#contenedor").trigger("load", {url: '/app/view/html/tercero/busqueda.html', name: 'Home'});
-        //$("#contenedor").trigger("load", {url: '/app/view/html/tercero/form.html', name: 'Home'});
+
+        $("#contenedor").trigger("load", {url: '/app/view/html/home.html', name: 'Home'});
+
     };
 
     $("#session").trigger("validation", action);
     
-    var session_validation = function(){$("#csession").trigger("validation", null);};
+    var session_validation = function(){$("#session").trigger("validation", null);};
     
     //setInterval(session_validation, 10000);
 
